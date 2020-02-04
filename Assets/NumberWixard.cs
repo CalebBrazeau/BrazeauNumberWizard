692e04898325773032a6953ddd8cfba20d2081b6
DontWorryAboutIt <brazea435@morrisville.edu>
2020-02-04 15:49:59 -0500

    added variables for max and min number

--- Assets/NumberWixard.cs
+++ Assets/NumberWixard.cs
@@ -7,10 +7,17 @@ public class NumberWixard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //max Number Variable
+        int intMaxNumber = 4;
+        //Min Number Variable
+        int intMinNumber = 1;
+
         Debug.Log("Welcome to number wizard");
         Debug.Log("Pick a number, do not tell me what it is");
-        Debug.Log("The highest number you can pick is 4");
-        Debug.Log("The lowest number you can pick is 1");
+        Debug.Log("The highest number you can pick is: " + intMaxNumber);
+        Debug.Log("The lowest number you can pick is: " + intMinNumber);
+        Debug.Log("Tell me if your number is higher or lower than 4");
+        Debug.Log("Push Up = higher, Push Down = lower, Push Enter = Correct");
     }
 
     // Update is called once per frame
