40e67ff17f36d23d3c57e36b13469acad983efd6
DontWorryAboutIt <brazea435@morrisville.edu>
2020-02-06 15:16:31 -0500

    Cleaned code up, made it so it can restart the game

--- Assets/NumberWixard.cs
+++ Assets/NumberWixard.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 public class NumberWixard : MonoBehaviour {
 
     //max Number Variable
-    int intMaxNumber = 10;
+    int intMaxNumber = 1000;
      //Min Number Variable
     int intMinNumber = 1;
     //Player guess
-    int intGuess = 5;
+    int intGuess = 500;
     // Start is called before the first frame update
     
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        intMaxNumber = 1000;
+        intMinNumber = 1;
+        intGuess = 500;
         //Talking to user
         Debug.Log("Welcome to number wizard");
         Debug.Log("Pick a number, do not tell me what it is");
         Debug.Log("The highest number you can pick is: " + intMaxNumber);
         Debug.Log("The lowest number you can pick is: " + intMinNumber);
-        Debug.Log("Tell me if your number is higher or lower than 4");
+        Debug.Log("Tell me if your number is higher or lower than " + intGuess);
         Debug.Log("Push Up = higher, Push Down = lower, Push Enter = Correct");
-
+        intMaxNumber = intMaxNumber + 1;        
     }
 
     // Update is called once per frame
@@ -30,26 +38,30 @@ public class NumberWixard : MonoBehaviour {
         //Input From player for up arrow key
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            //prints this when player presses up arrow key
-            Debug.Log("Up arrow key was pressed");
             //Changes minimun number to the players guess
             intMinNumber = intGuess;
-            Debug.Log(intGuess);
+            NextGuess();
         }
         //Input from player for down key
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            //prints this when player presses Down arrow key
-            Debug.Log("Down arrow key was pressed");
             //Changes Maximum number to players guess
             intMaxNumber = intGuess;
-            Debug.Log(intGuess);
+            NextGuess();
         }
         //Input from player for Enter Key
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             //prints this when player presses Enter Key
-            Debug.Log("Enter key was pressed");
+            Debug.Log("I win");
+            StartGame();
         }
+    }
+    void NextGuess()
+    {
+
+            //New guess
+            intGuess = (intMaxNumber + intMinNumber) / 2;
+            Debug.Log("Tell me if your number is higher or lower than " + intGuess);
     }
 }
