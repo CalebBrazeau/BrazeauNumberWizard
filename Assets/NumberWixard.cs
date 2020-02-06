e056c5b9a47ab36d4e31ae422ffaeeed43fb498e
DontWorryAboutIt <brazea435@morrisville.edu>
2020-02-06 14:59:42 -0500

    Changes guess to min or max number based on player input

--- Assets/NumberWixard.cs
+++ Assets/NumberWixard.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NumberWixard : MonoBehaviour
-{
+public class NumberWixard : MonoBehaviour {
+
+    //max Number Variable
+    int intMaxNumber = 10;
+     //Min Number Variable
+    int intMinNumber = 1;
+    //Player guess
+    int intGuess = 5;
     // Start is called before the first frame update
+    
     void Start()
     {
-        //max Number Variable
-        int intMaxNumber = 4;
-        //Min Number Variable
-        int intMinNumber = 1;
-
         //Talking to user
         Debug.Log("Welcome to number wizard");
         Debug.Log("Pick a number, do not tell me what it is");
@@ -30,12 +32,18 @@ public class NumberWixard : MonoBehaviour
         {
             //prints this when player presses up arrow key
             Debug.Log("Up arrow key was pressed");
+            //Changes minimun number to the players guess
+            intMinNumber = intGuess;
+            Debug.Log(intGuess);
         }
         //Input from player for down key
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             //prints this when player presses Down arrow key
             Debug.Log("Down arrow key was pressed");
+            //Changes Maximum number to players guess
+            intMaxNumber = intGuess;
+            Debug.Log(intGuess);
         }
         //Input from player for Enter Key
         else if (Input.GetKeyDown(KeyCode.Return))
