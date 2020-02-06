8f6242a6adbfb300c21183cb638ae54a9647c500
DontWorryAboutIt <brazea435@morrisville.edu>
2020-02-06 14:41:53 -0500

    Added user input for up arrow, down arrow, and enter key

--- Assets/NumberWixard.cs
+++ Assets/NumberWixard.cs
@@ -12,17 +12,36 @@ public class NumberWixard : MonoBehaviour
         //Min Number Variable
         int intMinNumber = 1;
 
+        //Talking to user
         Debug.Log("Welcome to number wizard");
         Debug.Log("Pick a number, do not tell me what it is");
         Debug.Log("The highest number you can pick is: " + intMaxNumber);
         Debug.Log("The lowest number you can pick is: " + intMinNumber);
         Debug.Log("Tell me if your number is higher or lower than 4");
         Debug.Log("Push Up = higher, Push Down = lower, Push Enter = Correct");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Input From player for up arrow key
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //prints this when player presses up arrow key
+            Debug.Log("Up arrow key was pressed");
+        }
+        //Input from player for down key
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            //prints this when player presses Down arrow key
+            Debug.Log("Down arrow key was pressed");
+        }
+        //Input from player for Enter Key
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //prints this when player presses Enter Key
+            Debug.Log("Enter key was pressed");
+        }
     }
 }
